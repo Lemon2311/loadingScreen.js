88b929970219a7d1f6bd792de2503bbfcc0d5a8e
Lemon2311 <marcotighiliu@yahoo.com>
2023-12-28 17:37:22 +0200

    initialized example

---
+++ script.js
@@ -0,0 +1,15 @@
+import { LoadingScreen } from './loadingScreen.js';
+
+const loadingScreen = new LoadingScreen();
+
+loadingScreen.hideOnJsLoad();
+
+performHeavyComputation();
+
+function performHeavyComputation() {
+    let result = 0;
+    for (let i = 0; i < 10000000; i++) {
+        result += Math.sin(i) * Math.cos(i);
+    }
+    return result;
+}
