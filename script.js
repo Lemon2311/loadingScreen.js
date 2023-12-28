f2a6b60bd1f3a047e51ebb4db2aaa12456e66d60
Marco Ulise Tighiliu <63803133+Lemon2311@users.noreply.github.com>
2023-12-28 17:56:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ performHeavyComputation();
 
 function performHeavyComputation() {
     let result = 0;
-    for (let i = 0; i < 50000000; i++) {
+    for (let i = 0; i < 100000000; i++) {
         result += Math.sin(i) * Math.cos(i);
     }
     return result;
